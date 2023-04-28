acccff3f2e4f29208aa39ef6afbe8c6b71a8c2c1
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-04-28 09:12:52 -0400

    folder structure setup

---
+++ controllers/api/projectRoutes.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const { Project } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newProject = await Project.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const readerData = await Reader.findByPk(req.params.id, {
+      include: [{ model: LibraryCard }, { model: Book }],
+      attributes: {
+        include: [
+          [
+            // Use plain SQL to get a count of all short books
+            sequelize.literal(
+              'SELECT * FROM characters WHERE title = "Groot";'
+            ),
+          ],
+        ],
+      },
+    });
+
+    if (!readerData) {
+      res.status(404).json({ message: 'No reader found with that id!' });
+      return;
+    }
+
+    res.status(200).json(readerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
